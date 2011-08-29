f8f2a1d92cd821e6555282a854e0e4df48b1957a
Rebecca Skinner <traybaby@gmail.com>
2011-08-29 12:29:05 +0800

    Cleaned up code for instantiating DB factories with associations - should be all happy!

--- tests/Phynaster/Zend/InstantiateFactoryTest.php
+++ tests/Phynaster/Zend/InstantiateFactoryTest.php
@@ -4,27 +4,86 @@ require_once 'Phynaster/Adapter/Zend.php';
 require_once 'Zend/Db/Table.php';
 require_once 'Zend/Db/Adapter/Pdo/Sqlite.php';
 
-$adapter = new Zend_Db_Adapter_Pdo_Sqlite(array('dbname' => 'test'));
-$adapter->query("DROP TABLE IF EXISTS test;");
-$adapter->query("CREATE TABLE test(id INTEGER PRIMARY KEY ASC, name VARCHAR(25))");
 
-class MemberInsertTable extends Zend_Db_Table
+class AssocTable extends Zend_Db_Table
 {
   protected $_primary = 'id';
-  protected $_name = 'test';
+  protected $_name = 'assoc';
 }
 
-Zend_Db_Table_Abstract::setDefaultAdapter($adapter);
+class BaseTable extends Zend_Db_Table
+{
+  protected $_primary = 'id';
+  protected $_name = 'base';
+}
 
 class InstantiateFactoryTest extends PhynasterTestCase
 {
+
+  public function setUp()
+  {
+    $adapter = new Zend_Db_Adapter_Pdo_Sqlite(array('dbname' => 'test'));
+    $adapter->query("DROP TABLE IF EXISTS assoc;");
+    $adapter->query("DROP TABLE IF EXISTS base;");
+    $adapter->query("CREATE TABLE base(id INTEGER PRIMARY KEY ASC, name VARCHAR(25))");
+    $adapter->query("CREATE TABLE assoc(id INTEGER PRIMARY KEY ASC, base_id INTEGER, name VARCHAR(25))");
+
+    Zend_Db_Table_Abstract::setDefaultAdapter($adapter);
+    return parent::setUp();
+  }
+
   public function testCanSaveDatabaseRecordByInstantiatingFactory()
   {
-    $table = new MemberInsertTable;
-    $factory = Phynaster::define('test', array('defaults' => array('name' => 'Test'), 'adapter' => Phynaster::adapter('Zend', $table)));
-    $instance = Phynaster::create('test');
+    $table = new BaseTable;
+    Phynaster::define('base', array('defaults' => array('name' => 'Test'), 'adapter' => Phynaster::adapter('Zend', $table)));
+    Phynaster::create('base');
 
     // DB table should now have a record.
     $this->assertEquals(count($table->fetchAll()), 1);
+  }
+
+  public function testCanSaveMultipleDatabaseRecordsByInstantiatingFactoryWithAssociation()
+  {
+    $assocTable = new AssocTable;
+    $baseTable = new BaseTable;
+
+    Phynaster::define('base', array('defaults' => array('name' => 'Test'), 'adapter' => Phynaster::adapter('Zend', $baseTable)));
+    Phynaster::define('assoc', array(
+      'defaults' => array('name' => 'Test', 'base_id' => Phynaster::association('base')),
+      'adapter' => Phynaster::adapter('Zend', $assocTable)));
+
+    Phynaster::create('assoc');
+
+    // Both DB tables should now have records.
+    $this->assertEquals(count($assocTable->fetchAll()), 1);
+    $this->assertEquals(count($baseTable->fetchAll()), 1);
+
+    // And the db value for base.test_id should equal the value for test.id
+    $baseRecord = $baseTable->fetchAll()->current();
+    $assocRecord = $assocTable->fetchAll()->current();
+    $this->assertEquals($assocRecord['base_id'], $baseRecord['id']);
+  }
+
+  public function testOverwritingAssociationDoesNotCreateMultipleDatabaseRecords()
+  {
+    // Very similar to previous test, except for...
+    $assocTable = new AssocTable;
+    $baseTable = new BaseTable;
+
+    Phynaster::define('base', array('defaults' => array('name' => 'Test'), 'adapter' => Phynaster::adapter('Zend', $baseTable)));
+    Phynaster::define('assoc', array(
+      'defaults' => array('name' => 'Test', 'base_id' => Phynaster::association('base')),
+      'adapter' => Phynaster::adapter('Zend', $assocTable)));
+
+    // This line!
+    Phynaster::create('assoc', array('base_id' => Phynaster::create('base', array('name' => 'Not the default'))));
+
+    // Both DB tables should now have records.
+    $this->assertEquals(count($assocTable->fetchAll()), 1);
+    $this->assertEquals(count($baseTable->fetchAll()), 1);
+
+    // And the created record for the base table should be the overwritten one, not the default
+    $baseRecord = $baseTable->fetchAll()->current();
+    $this->assertEquals('Not the default', $baseRecord['name']);
   }
 }
